0e74118a9b9f3e55f7cb6298888f23625fd4a0f9
RichardJohnRx <the.cool.guy492@gmail.com>
2021-03-22 09:57:15 +0100

    Ajout de commentaires - partie 1

--- src/App.vue
+++ src/App.vue
@@ -14,22 +14,38 @@ export default {
     }
   },
   mounted(){
+    //On envoie un message dans la console indiquant que l'appli est démarrée
     console.log("L'app est demarrée")
+    //On envoie une requête à l'API pour savoir si la connection est OK
     api.get('ping').then(reponse => {
+      //Si oui on change la visualization de l'appli à TRUE
+      // et on envoie une message dans la console
       this.apiOk = true;
       console.log("L'api est fonctionnelle");
 
-      if(!this.$store.state.membre || !this.$store.state.token){
+      //Si à l'intérieur de l'appli n'est pas sauvegardé la variable
+      // du membre connecté et son token...
+      if(!this.$store.state.membre && !this.$store.state.token){
+        //Et si le path de la route est different de 'se-connecter' et 'creer-compte'
         if(this.$route.path !== '/se-connecter' || this.$route.path !== '/creer-compte') {
+          //L'appli est redirigée vers la route 'se-connecter'
           this.$router.push('/se-connecter');
         }
       } else {
+        //Sinon l'appli est redirigée vers la route 'conversations'
         this.$router.push('/conversations');
       }
       //this.$router.push('/se-connecter');
     }).catch(error => {
+      //Si la requête a des erreurs de connexion on envoie
+      //une message d'erreur dans la console
       console.log("L'api ne marche pas")
     });
+
+    //On ajout des méthodes dans la variable $bus pour qu'ils soient
+    //disponibles dans tout l'environnement de l'appli
+
+    //Le methode
     this.$bus.$on('charger-conversations',this.chargerConversations);
     this.$bus.$on('charger-membres',this.chargerMembres);
   },
--- src/main.js
+++ src/main.js
@@ -9,16 +9,21 @@ import 'semantic-ui-css/semantic.min.css';
 Vue.use(SuiVue);
 
 window.api = axios.create({
+  //L'URL de l'API Coop
   baseURL: 'https://allweb.fun/coop/api/',
+  //On ajout le token authorization dans le header de chaque requête
   headers: { Authorization: '04e755be07aaaf9fae6a9618873b9332abefd167' }
 });
 
 Vue.config.productionTip = false
 
+//On crée une variable nommé $bus qui va mettre à disposition
+//des fonctions dans tout l'environnement de l'appli
 Vue.prototype.$bus = new Vue()
 
 new Vue({
   beforeCreate() {
+    //On ajoute le token du membre connecté pour réaliser le CRUD de chaque module
     api.interceptors.request.use(config => {
       if (this.$store.state.token){
         config.url += '?token=' + this.$store.state.token;
